bb36048e5ce7cddeb59d6a893308aa3a70efbad7
jeynerarana <jeyner.arana@gmail.com>
2022-06-08 14:26:01 -0700

    added function to send message

--- SeniorProject.py
+++ SeniorProject.py
@@ -9,15 +9,7 @@ from scapy.all import sniff
 # Find your Account SID and Auth Token at twilio.com/console
 # and set the environment variables. See http://twil.io/secure
 # Start
-account_sid = "ACf03feeb2b0e2e3e6027c1356b645d022"
-auth_token = "013160597fed8fc537d0eabda330650d"
-client = Client(account_sid, auth_token)
-message = client.messages \
-    .create(
-         body='Hi Cunt',
-         from_='+14784296043',
-         to='+13235097091'
-     )
+
 # Print out the first 5 packets
 # if __name__=="main":
 #      print("hello")
@@ -25,3 +17,17 @@ message = client.messages \
 # print("Testing")
 # cap = sniff()
 # cap.summary()
+
+# Let's make a function that sends a message to your phone
+def sndMessage(b_message):
+    # TODO
+    account_sid = "ACf03feeb2b0e2e3e6027c1356b645d022"
+    auth_token = "013160597fed8fc537d0eabda330650d"
+    client = Client(account_sid, auth_token)
+    message = client.messages \
+    .create(
+         body=b_message,
+         from_='+14784296043',
+         to='+13235097091'
+     )
+sndMessage('hello')

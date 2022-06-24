747ed076b63113b9f345530a9af33307136497cf
Jeyner <jeyner.arana@gmail.com>
2022-06-24 11:56:07 -0700

    Cleaned up SenionProject.py

--- SeniorProject.py
+++ SeniorProject.py
@@ -6,19 +6,6 @@ from scapy.all import sniff
 #Getting used to git
 #Editing via Github browser
 
-
-# Find your Account SID and Auth Token at twilio.com/console
-# and set the environment variables. See http://twil.io/secure
-# Start
-
-# Print out the first 5 packets
-# if __name__=="main":
-#      print("hello")
-#      print(sniff())
-# print("Testing")
-# cap = sniff()
-# cap.summary()
-
 # Let's make a function that sends a message to your phone
 def sndMessage(b_message):
     account_sid = "ACf03feeb2b0e2e3e6027c1356b645d022"
@@ -31,7 +18,3 @@ def sndMessage(b_message):
          to='+13235097091'
      )
 sndMessage('IDS is running')
-
-#def capPackets(count):
-#    capture = sniff(pac_Number = count)
-#    capture.summary()
